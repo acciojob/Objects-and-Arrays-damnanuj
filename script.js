1a85afe2a1f04a3b8a4366fbf2b524710766cfe5
Anuj <106218844+damnanuj@users.noreply.github.com>
2024-03-21 22:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const person = {
   name: "John Doe",
   age: 80,
 };
+const team = players;
+const team1 = [...players]; // Create a shallow copy of the players array
+const cap1 = Object.assign({}, person); // Create a shallow copy of the person object
+
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
